a5f5503197d9120230a7e9d060be18a2d1b0cb28
Fellzo <gely902@gmail.com>
2017-03-26 23:16:31 +0300

    Update config.php

--- public/polls/config.php
+++ public/polls/config.php
@@ -3,9 +3,9 @@
       "db_config" => [
           "driver" => "mysql",
           "host" => "localhost",
-          "database" => "c5483_polls",
-          "username" => "c5483_admin",
-          "password" => "HdRew5412+kJhG*2",
+          "database" => "db",
+          "username" => "log",
+          "password" => "pass",
           "charset"   => "utf8",
           "collation" => "utf8_general_ci",
           "prefix"    => "",
